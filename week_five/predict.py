96a0d4de1f1ce3ad81fc88e9dd603ad50795381d
Gregory W Morris <gwm1980@gmail.com>
2022-10-03 11:35:20 -0400

    predict python file

---
+++ week_five/predict.py
@@ -0,0 +1,39 @@
+"""## Load the model"""
+
+input_file = 'model_C=1.0.bin' # file location
+
+with open(input_file, 'rb') as f_in: # rb = read bin
+    dv, model = pickle.load(f_in) 
+# dv is needed so you can transform input, otherwise the model will not be able to proccess the imput
+
+model
+
+customer = {
+    'gender': 'female',
+    'seniorcitizen': 0,
+    'partner': 'yes',
+    'dependents': 'no',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'month-to-month',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 1,
+    'monthlycharges': 29.85,
+    'totalcharges': 29.85
+}
+# new customer to test model
+
+X = dv.transform([customer])
+
+y_pred = model.predict_proba(X)[0, 1]
+
+print('input:', customer)
+print('output:', y_pred)
