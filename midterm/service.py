63825cd574b10a0363923930672de7eee100b514
Gregory W Morris <gwm1980@gmail.com>
2023-04-24 19:50:40 -0400

    Update service.py

--- midterm/service.py
+++ midterm/service.py
@@ -10,7 +10,20 @@ model_runner = model_ref.to_runner()
 
 svc = bentoml.Service('stroke_prediction', runners=[model_runner])
 
-@svc.api(input= JSON() , output= JSON())
+@svc.api(input= JSON.from_sample({
+        "gender": "Male",
+        "age": 69.0,
+        "hypertension": 0,
+        "heart_disease": 1,
+        "ever_married": 1,
+        "work_type": "self_employed",
+        "residence_type": "Urban",
+        "avg_glucose_level": 195.23,
+        "bmi": 28.3,
+        "smoking_status": "smokes",
+        "obese": 0,
+        "clearly_diabetes": 1
+        }), output= JSON())
 def classify(application_data):
     vector = dv.transform(application_data)
     prediction = model_runner.predict_proba.run(vector)
